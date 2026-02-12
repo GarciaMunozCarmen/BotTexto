756713af014b14da20cc13cb3a4c7c6e5bd66ed6
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-02-12 09:06:03 +0100

    trabajo en Casa

---
+++ src/main/java/es/fplumara/dam1/pruebaBot/model/LogEntry.java
@@ -0,0 +1,11 @@
+package es.fplumara.dam1.pruebaBot.model;
+
+import java.time.LocalDateTime;
+
+public record LogEntry(
+        LocalDateTime timestamp,
+        String author,
+        String content,
+        long channelId) {
+
+}
